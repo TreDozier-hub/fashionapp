49e00ace957daa15d279703b6283451cc7804f74
TreDozier-hub <arnaud.dozier@gmail.com>
2024-11-16 17:36:52 +0100

    Update footer & css

--- fashionapp/fashion-app/src/components/Footer.js
+++ fashionapp/fashion-app/src/components/Footer.js
@@ -2,40 +2,75 @@ import React from 'react';
 import Logo from './assets/logo_fashion.png';
 import SignButton from './SignButton.js';
 import './App.css';
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaBeer } from 'react-icons/fa';
 
 
 function NavBar() {
     return (
-        <nav className="footer">
-            <section>
-            <div className="footerLogo">
+
+        <footer class="footer">
+            <div class="footer-container">
+                <div class="footer-section">
+                    <h3 class="footer-brand">FASHION</h3>
+                    {/* <div className="navbarLogo">
                 <a  href="#logo">
                     <img src={Logo} alt="logo" className="navbarLogo" />
                 </a>
-            </div> 
-            <p>Complete your style with awesome clothes from us.</p>
-            <div class="sociaux">
-                
+            </div> */}
+                    <p class="footer-description">
+                        Complete your style with awesome clothes from us.
+                    </p>
+                    <div class="footer-socials">
+                        <FaFacebook />
+                        <RiInstagramFill />
+                        <FaSquareXTwitter />
+                        <FaLinkedin />
+                    </div>
+                </div>
+
+
+                <div class="footer-section">
+                    <h4>Company</h4>
+                    <ul class="footer-links">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-section">
+                    <h4>Quick Link</h4>
+                    <ul class="footer-links">
+                        <li><a href="#">Share Location</a></li>
+                        <li><a href="#">Orders Tracking</a></li>
+                        <li><a href="#">Size Guide</a></li>
+                        <li><a href="#">FAQs</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-section">
+                    <h4>Legal</h4>
+                    <ul class="footer-links">
+                        <li><a href="#">Terms & conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
             </div>
-            </section>
-            <div className="footerLogo">
-                <a  href="#logo">
-                    <img src={Logo} alt="logo" className="navbarLogo" />
-                </a>
-            </div>
-            <ul className="navbar--link">
-                <a className="navbar--link-item" href="#catalogue">Catalogue</a>
-                <a className="navbar--link-item" href="#fashion">Fashion</a>
-                <a className="navbar--link-item" href="#favourite">Favourite</a>
-                <a className="navbar--link-item"href="#lifestyle">Lifestyle</a>
-            </ul>
-            <div className="navbar--signUp">
-                <>
-                    <SignButton />
-                </>
-            </div>
-        </nav>
+        </footer>
+
+
     );
+
 }
 
+// class Question extends React.Component {
+//     render() {
+//         return <h3> Lets go for a <FaBeer />? </h3>
+//     }
+// }
 export default NavBar; 
